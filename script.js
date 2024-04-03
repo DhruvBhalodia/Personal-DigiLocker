cbdefc64c0f3442bcfa86ddf7de72970d616c1c1
DhruvBhalodia <dhruvcoder007@gmail.com>
2024-04-03 18:34:02 +0530

    script.js modified accordingly

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-document.querySelector('.container').addEventListener('submit', (event) => {
+document.querySelector('.container').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const username = event.target.elements.uname.value;
     const password = event.target.elements.psw.value;
 
-    fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        if (data.success) {
-            console.log("Success");
+    try {
+        const response = await fetch(`http://localhost:3000/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log("Success:", data.message);
         } else {
-            console.log("Fail");
+            console.log("Fail:", data.message);
         }
-    });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
